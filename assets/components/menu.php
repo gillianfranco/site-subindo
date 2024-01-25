a1427a67e9dfce042fcae248e5979ca006c16e7a
gillianfranco <gillian.melo.franco17@gmail.com>
2024-01-25 09:42:28 -0300

    Elaborar título do Above The Fold

---
+++ assets/components/menu.php
@@ -0,0 +1,13 @@
+<nav>
+    <div>
+        <a href=""><img src="assets/images/logos/logo-subindo900x248.png" alt="Logo Subindo Agência"></a>
+    </div>
+    <ul>
+        <li><a href=""><p class="color-vermelho">Empresa</p></a></li>
+        <li><a href="#header_servicos"><p class="color-vermelho">Serviços</p></a></li>
+        <li><a href=""><p class="color-vermelho">Contato</p></a></li>
+    </ul>
+</nav>
+<nav id="header_servicos">
+    
+</nav>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <link rel="shortcut icon" href="assets/images/favicon/favicon.png">
+
+    <title>Subindo | Agência de Marketing Digital</title>
+</head>
+    <body>
+        <main>
+            <header class="background-preto">
+                <?php include "assets/components/menu.php"; ?>
+            </header>
+            <section>
+                <div id="container__above_the_fold" class="background-preto">
+                    <div class="padding-sessoes alinhamento-linha centralizar">
+                        <div class="background-cinzaClaro">
+                            
+                        </div>
+                        <div class="">
+                            <img src="assets/images/logos/logo-titulo900x360.png" alt="Logo Subindo Agência" id="titulo__above_the_fold" style="width: 26em;">
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <footer></footer>
+        </main>
+    </body>
+</html>
