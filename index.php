7b043b6499593a4058aadf1b517ed9545d68978e
gillianfranco <gillian.melo.franco17@gmail.com>
2024-01-25 14:04:38 -0300

    Criar campo sobre o objetivo da empresa

--- index.php
+++ index.php
@@ -16,14 +16,24 @@
                 <?php include "assets/components/menu.php"; ?>
             </header>
             <section>
-                <div id="container__above_the_fold" class="background-preto">
-                    <div class="padding-sessoes alinhamento-linha centralizar">
-                        <div class="background-cinzaClaro">
-                            
-                        </div>
-                        <div class="">
-                            <img src="assets/images/logos/logo-titulo900x360.png" alt="Logo Subindo Agência" id="titulo__above_the_fold" style="width: 26em;">
-                        </div>
+                <div id="container__above_the_fold" class="background-preto alinhamento-linha centralizar">
+                    <div class="centralizar">
+                        <abbr title="https://pt.vecteezy.com/vetor-gratis/marketing">
+                            <img src="assets/images/imagem-above-the-fold/imagem-above-the-fold.png" alt="" style="width: 88vh;">
+                        </abbr>
+                    </div>
+                    <div>
+                        <img src="assets/images/logos/logo-titulo900x360.png" alt="Logo Subindo Agência" id="titulo__above_the_fold" style="width: 88vh;">
+                    </div>
+                </div>
+                <div id="container__sobre_nos" class="padding-sessoes background-preto alinhamento-linha centralizar">
+                    <div class="centralizar alinhamento-coluna">
+                        <h2 class="color-vermelho">Sobre Nós</h2>
+                        <p class="color-branco">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae debitis rerum alias saepe tempora delectus ab sit facilis velit officiis unde aut eos aperiam dignissimos, corrupti perspiciatis molestiae, omnis sapiente. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas provident nostrum consectetur nesciunt qui nulla impedit necessitatibus, maiores placeat? Ea commodi enim non ducimus repudiandae vero natus? Corporis, quas blanditiis?</p>
+                        <a href="" class="btn">Conheça a Empresa</a>
+                    </div>
+                    <div class="color-vermelho background-cinzaClaro centralizar">
+                        <img src="assets/images/fotos/foto-estudio1275x1002.png" alt="Foto do Estúdio" style="width: 75vh;">
                     </div>
                 </div>
             </section>
